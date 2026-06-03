cf295e4ab4bebf9deac2a8e60ab895785691efb4
sangjun <etkdwns@gmail.com>
2026-06-03 20:25:57 +0900

    feat: 본인 계정 테스트 배포 준비

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/digicon-curator/",
+  base: "/digicon_culture_test/",
 })
